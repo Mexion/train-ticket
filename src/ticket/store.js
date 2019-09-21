e5460994d3e6c609253b084256c8cad84ae16e90
Mexion <1095705440@qq.com>
2019-09-22 00:17:58 +0800

    save before ejecting

---
+++ src/ticket/store.js
@@ -0,0 +1,9 @@
+import reducers from './reducers';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+export default createStore(
+    combineReducers(reducers),
+    {},
+    applyMiddleware(thunk)
+);
