4c63c5e6640522d3830d8811c900306af2d31c47
Mexion <1095705440@qq.com>
2019-10-10 01:47:38 +0800

    Judge to open serviceWorker

--- src/query/index.js
+++ src/query/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import "normalize.css/normalize.css";
+import * as serviceWorker from "../serviceWorker";
 
 import store from "./store/";
 import App from "./App";
@@ -14,3 +15,10 @@ ReactDOM.render(
     </Provider>,
     document.getElementById("root")
 );
+
+//判断是否是生产环境，是否开启SW
+if ("production" === process.env.NODE_ENV) {
+    serviceWorker.register();
+} else {
+    serviceWorker.unregister();
+}
